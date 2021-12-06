a2048646111383bb154352f65b50df17776d80be
dotnotation <dotinmotionproductions@gmail.com>
2021-12-06 14:11:31 -0500

    commit tree aee315923be8271810c2c360949b4312644ca7c3

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(sourceArray, work){
+    let mapped = [];
+    sourceArray.forEach(element => mapped.push(work(element)));
+    return mapped;
+}
+
+function reduce(sourceArray, memo, startingPoint){
+    let reduced;
+    if (startingPoint){
+        reduced = startingPoint;
+        sourceArray.forEach(element => reduced = memo(element, reduced));
+    } else {
+        reduced = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++){
+            reduced = memo(sourceArray[i], reduced)
+        };
+    }
+    return reduced; 
+}
